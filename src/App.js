d1451cf5bded0f89215ee81e53a91d298aa9b3d2
Gordon Jooste <jooste44@gmail.com>
2022-08-19 12:34:45 +0700

    inital commit of the challenge project after downloading the starting files

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { Switch, Route, Redirect, NavLink } from "react-router-dom";
+
+import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
+import { ContactsPage } from "./containers/contactsPage/ContactsPage";
+
+function App() {
+
+ const [contacts, setContacts] = useState([]);
+ const [appointments, setAppointments] = useState([]);
+
+  const ROUTES = {
+    CONTACTS: "/contacts",
+    APPOINTMENTS: "/appointments",
+  };
+
+  const addContact = (name, phoneNumber, email)=>{
+    setContacts((prev)=>{
+      [...prev,
+      {name: name,
+      phoneNumber: phoneNumber,
+      email: email}
+      ]
+    });
+  }
+
+  const addAppointment = (title, contact, date, time)=>{
+    setAppointments((prev)=>{
+      [
+        ...prev,
+        {
+          title: title,
+          contact: contact,
+          date: date,
+          time: time,
+        }
+      ]
+    });
+  }
+
+
+
+  return (
+    <>
+      <nav>
+        <NavLink to={ROUTES.CONTACTS} activeClassName="active">
+          Contacts
+        </NavLink>
+        <NavLink to={ROUTES.APPOINTMENTS} activeClassName="active">
+          Appointments
+        </NavLink>
+      </nav>
+      <main>
+        <Switch>
+          <Route exact path="/">
+            <Redirect to={ROUTES.CONTACTS} />
+          </Route>
+          <Route path={ROUTES.CONTACTS}>
+             {/* Add props to ContactsPage */}
+            <ContactsPage />
+          </Route>
+          <Route path={ROUTES.APPOINTMENTS}>
+            {/* Add props to AppointmentsPage */}
+            <AppointmentsPage />
+          </Route>
+        </Switch>
+      </main>
+    </>
+  );
+}
+
+export default App;
